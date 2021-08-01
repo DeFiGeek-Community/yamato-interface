2b6deb0b0f7ea2767dd01a6689cc5a7e5ac60c90
0xTeaTwo <0xteatwo@protonmail.com>
2021-08-01 18:23:59 +0900

    transplant component

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+
+declare module '@metamask/jazzicon' {
+  export default function (diameter: number, seed: number): HTMLElement;
+}
+
+declare module 'fortmatic';
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: true;
+    on?: (...args: any[]) => void;
+    removeListener?: (...args: any[]) => void;
+    autoRefreshOnNetworkChange?: boolean;
+  };
+  web3?: Record<string, unknown>;
+}
