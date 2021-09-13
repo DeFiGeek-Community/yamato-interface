86dd2c8b1bc9311645409f0abf859b9b97e79370
typingmonkey <panebinese@gmail.com>
2021-09-13 16:07:15 +0900

    feat(tools): add a page, tools

---
+++ src/pages/tools.tsx
@@ -0,0 +1,16 @@
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet';
+// import { Link } from 'react-router-dom';
+
+export default function Tool() {
+  return (
+    <>
+      <Box>
+        <Helmet title="Yamato Interface: Tools" />
+        <Grid>
+          <GridItem>hoge</GridItem>
+        </Grid>
+      </Box>
+    </>
+  );
+}
