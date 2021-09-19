229e066ed9ce94ec1a5f6863dc8d5285558cf569
typingmonkey <panebinese@gmail.com>
2021-09-20 01:56:24 +0900

    feat(tools): put a Infographics component onto tools page

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -1,3 +1,4 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Helmet } from 'react-helmet';
 import InfographicsTool from '../components/InfographicsTool';
 import Layout from '../components/Layout';
@@ -6,7 +7,15 @@ export default function Tools() {
   return (
     <Layout>
       <Helmet title="Yamato Tools" />
-      <InfographicsTool />
+      <Grid
+        templateRows="repeat(8, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        gap={8}
+      >
+        <GridItem rowSpan={5} colSpan={2}>
+          <InfographicsTool />
+        </GridItem>
+      </Grid>
     </Layout>
   );
 }
