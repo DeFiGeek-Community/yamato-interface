4c280558065e5723ce2b98b36c491df3c7dfd585
typingmonkey <panebinese@gmail.com>
2021-09-20 01:30:26 +0900

    feat(tools): fix style

--- src/components/InfographicsTool.tsx
+++ src/components/InfographicsTool.tsx
@@ -1,14 +1,13 @@
-import {
-  Grid,
-  GridItem,
-  Stack,
-  NumberInput,
-  NumberInputField,
-  FormControl,
-  FormLabel,
-} from '@chakra-ui/react';
+import { Grid, GridItem, Stack, FormControl } from '@chakra-ui/react';
 import { Formik, Form } from 'formik';
 import Infographics from '../components/Infographics';
+import {
+  CategoryTitle,
+  ConentBox,
+  HeaderBox1,
+  CustomFormLabel,
+  CustomInput,
+} from './CommonItem';
 
 function InputField(props: {
   name: string;
@@ -20,15 +19,14 @@ function InputField(props: {
   const onChangeHandler = onChange
     ? onChange
     : (e: any) => {
-        formik.setFieldValue(name, parseFloat(e.target.value));
+        const val = parseFloat(e.target.value);
+        formik.setFieldValue(name, Number.isNaN(val) ? undefined : val);
       };
 
   return (
     <FormControl>
-      <FormLabel htmlFor={name}>{label}</FormLabel>
-      <NumberInput>
-        <NumberInputField id={name} onChange={onChangeHandler} />
-      </NumberInput>
+      <CustomFormLabel text={label} />
+      <CustomInput id={name} onChange={onChangeHandler} />
     </FormControl>
   );
 }
@@ -47,61 +45,67 @@ export default function InfographicsTool() {
               <Infographics {...formik.values} />
             </GridItem>
             <GridItem rowSpan={5} colSpan={1}>
-              <Stack>
-                <InputField
-                  name="totalCollateral"
-                  label="担保総額(ETH)"
-                  formik={formik}
-                />
-                <InputField
-                  name="totalDebt"
-                  label="借入総額(CJPY)"
-                  formik={formik}
-                />
-                <InputField
-                  name="rateOfCjpyJpy"
-                  label="CJPY/JPY 交換レート"
-                  formik={formik}
-                  onChange={(e: any) => {
-                    formik.setFieldValue(
-                      'rateOfCjpyJpy',
-                      '' == e.target.value
-                        ? undefined
-                        : { manual: parseFloat(e.target.value) }
-                    );
-                  }}
-                />
-                <InputField
-                  name="rateOfEthJpy"
-                  label="ETH/JPY 交換レート"
-                  formik={formik}
-                />
-                <InputField
-                  name="prevRateOfEthJpy"
-                  label="直前の ETH/JPY 交換レート"
-                  formik={formik}
-                />
-                <InputField
-                  name="redemptionReserve"
-                  label="償還プール(CJPY)"
-                  formik={formik}
-                />
-                <InputField
-                  name="prevRedemptionReserve"
-                  label="直前の償還プール(CJPY)"
-                  formik={formik}
-                />
-                <InputField
-                  name="sweepReserve"
-                  label="弁済プール(CJPY)"
-                  formik={formik}
-                />
-                <InputField
-                  name="prevSweepReserve"
-                  label="直前の弁済プール(CJPY)"
-                  formik={formik}
-                />
-              </Stack>
+              <HeaderBox1>
+                <CategoryTitle>Manipulate Parameters</CategoryTitle>
+              </HeaderBox1>
+              <ConentBox>
+                <Stack>
+                  <InputField
+                    name="totalCollateral"
+                    label="担保総額(ETH)"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="totalDebt"
+                    label="借入総額(CJPY)"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="rateOfCjpyJpy"
+                    label="CJPY/JPY 交換レート"
+                    formik={formik}
+                    onChange={(e: any) => {
+                      const val = parseFloat(e.target.value);
+                      formik.setFieldValue(
+                        'rateOfCjpyJpy',
+                        '' == e.target.value
+                          ? undefined
+                          : { manual: Number.isNaN(val) ? undefined : val }
+                      );
+                    }}
+                  />
+                  <InputField
+                    name="rateOfEthJpy"
+                    label="ETH/JPY 交換レート"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="prevRateOfEthJpy"
+                    label="直前の ETH/JPY 交換レート"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="redemptionReserve"
+                    label="償還プール(CJPY)"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="prevRedemptionReserve"
+                    label="直前の償還プール(CJPY)"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="sweepReserve"
+                    label="弁済プール(CJPY)"
+                    formik={formik}
+                  />
+                  <InputField
+                    name="prevSweepReserve"
+                    label="直前の弁済プール(CJPY)"
+                    formik={formik}
+                  />
+                </Stack>
+              </ConentBox>
             </GridItem>
           </Grid>
         </Form>
