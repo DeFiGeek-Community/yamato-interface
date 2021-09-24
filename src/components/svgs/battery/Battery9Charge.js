1466991c94f842a902a157e1d26e97252931c65a
0xTeaTwo <0xteatwo@protonmail.com>
2021-09-25 00:38:35 +0900

    refactor: battery svgs

--- src/components/svgs/battery/Battery9Charge.js
+++ src/components/svgs/battery/Battery9Charge.js
@@ -3,45 +3,20 @@ import * as React from 'react';
 function SvgBattery9Charge(props) {
   return (
     <svg
-      width={63}
-      height={33}
+      width={62}
+      height={32}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g filter="url(#battery9_charge_svg__filter0_d)">
-        <rect width={60} height={32} rx={5} fill="#CFCFCF" />
-        <rect x={52} y={9} width={10} height={13} rx={1} fill="#CFCFCF" />
-        <rect x={5} y={5} width={50} height={22} rx={1} fill="#FCFAF2" />
-        <rect x={5} y={5} width={40} height={22} rx={1} fill="#5BAD92" />
-      </g>
+      <rect width={60} height={32} rx={5} fill="#CFCFCF" />
+      <rect x={52} y={9} width={10} height={13} rx={1} fill="#CFCFCF" />
+      <rect x={5} y={5} width={50} height={22} rx={1} fill="#FCFAF2" />
+      <rect x={5} y={5} width={40} height={22} rx={1} fill="#5BAD92" />
       <path
         d="M35 15.097l-4.907-.885L32.77 6 25 16.478l4.312.956L26.524 26 35 15.097z"
         fill="#FFE694"
       />
-      <defs>
-        <filter
-          id="battery9_charge_svg__filter0_d"
-          x={0}
-          y={0}
-          width={63}
-          height={33}
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dx={1} dy={1} />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
-          <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
-        </filter>
-      </defs>
     </svg>
   );
 }
