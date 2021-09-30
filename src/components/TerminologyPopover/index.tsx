30598cb24dc2de264c611995d05592c21bdd1aa6
typingmonkey <panebinese@gmail.com>
2021-10-01 00:32:56 +0900

    style: update style of TerminologyPopover

--- src/components/TerminologyPopover/index.tsx
+++ src/components/TerminologyPopover/index.tsx
@@ -8,7 +8,12 @@ import {
   PopoverCloseButton,
   PopoverHeader,
 } from '@chakra-ui/react';
-import { ButtonInHeaderBox1, ItemTitleForPledge } from '../CommonItem';
+import React from 'react';
+import {
+  ButtonInHeaderBox1,
+  headerBox1Color,
+  ItemTitleForPledge,
+} from '../CommonItem';
 
 export default function TerminologyPopover({
   title,
@@ -18,17 +23,27 @@ export default function TerminologyPopover({
   children: React.ReactNode;
 }) {
   return (
-    <Popover>
+    <Popover matchWidth={true}>
       <PopoverTrigger>
         <ButtonInHeaderBox1>?</ButtonInHeaderBox1>
       </PopoverTrigger>
       <PopoverContent>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverHeader>
+        <PopoverArrow bgColor={headerBox1Color} />
+        <PopoverCloseButton textColor={headerBox1Color} />
+        <PopoverHeader
+          borderColor={headerBox1Color}
+          borderWidth={'0.4em 0.4em 0.2em 0.4em'}
+          borderRadius={'10px 10px 0 0'}
+        >
           <ItemTitleForPledge>{title ?? '用語解説'}</ItemTitleForPledge>
         </PopoverHeader>
-        <PopoverBody>{children}</PopoverBody>
+        <PopoverBody
+          borderColor={headerBox1Color}
+          borderWidth={'0.2em 0.4em 0.4em 0.4em'}
+          borderRadius={'0 0 10px 10px'}
+        >
+          {children}
+        </PopoverBody>
       </PopoverContent>
     </Popover>
   );
