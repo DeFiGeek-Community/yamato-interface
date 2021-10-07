e5b325ff056c7ef77f71a6873a0f538ec6ab570a
0xTeaTwo <0xteatwo@protonmail.com>
2021-10-07 20:21:45 +0900

    chore: update contract address and abis

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -10,15 +10,15 @@ type AddressMap = { [chainId: number]: string };
 export type YamatoContractSort = 'main' | 'pool' | 'pricefeed';
 export const YAMATO_MAIN_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-  [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.RINKEBY]: '0xaD53F1AE38ef387BcE81B91Fbaf9A930dd887f9D',
 };
 export const YAMATO_POOL_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-  [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.RINKEBY]: '0x18ee71734e1ADdDC5709C68ED0FcFfbB8431D5f2',
 };
 export const YAMATO_PRICE_FEED_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-  [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.RINKEBY]: '0xfec905bAa07DAF770863E813EE02e843672C3E37',
 };
 
 /**
@@ -26,7 +26,7 @@ export const YAMATO_PRICE_FEED_ADDRESSES: AddressMap = {
  */
 export const CJPY_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-  [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.RINKEBY]: '0x47D39F3967887e6B45151a24da5ab4129237BE53',
 };
 export const YMT_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
