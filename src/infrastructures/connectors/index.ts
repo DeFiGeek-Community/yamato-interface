f133b2645fbc5bf1de38f02ecb0f2f6b2cc6d501
typingmonkey <panebinese@gmail.com>
2021-10-18 00:15:02 +0900

    test(e2e): add hardhat network into NETWORK_URLS

--- src/infrastructures/connectors/index.ts
+++ src/infrastructures/connectors/index.ts
@@ -34,6 +34,7 @@ const NETWORK_URLS: { [key in SupportedChainId]: string } = {
   [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
+  [SupportedChainId.HARDHAT]: 'http://127.0.0.1:8545/',
 };
 
 export const network = new NetworkConnector({
