b0023675e0881c5f0de36492843f732a3d884364
typingmonkey <panebinese@gmail.com>
2021-10-18 00:14:25 +0900

    test(e2e): add addresses for hardhat network

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -11,14 +11,17 @@ export type YamatoContractSort = 'main' | 'pool' | 'pricefeed';
 export const YAMATO_MAIN_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.RINKEBY]: '0x142984cEbE19b79fDdBAEFe7a6d8bc4C0204086D',
+  [SupportedChainId.HARDHAT]: '0x0dcd1bf9a1b36ce34237eeafef220932846bcd82',
 };
 export const YAMATO_POOL_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.RINKEBY]: '0x72D939E9d36AF8dFB5aCc6EB2bb13259D816598B',
+  [SupportedChainId.HARDHAT]: '0x9a676e781a523b5d0c0e43731313a708cb607508',
 };
 export const YAMATO_PRICE_FEED_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.RINKEBY]: '0xf513e1Aef21dE30696436fd3D9D8f196d0415B3d',
+  [SupportedChainId.HARDHAT]: '0xa513e6e4b8f2a923d98304ec87f64353c4d5c853',
 };
 
 /**
@@ -27,6 +30,7 @@ export const YAMATO_PRICE_FEED_ADDRESSES: AddressMap = {
 export const CJPY_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   [SupportedChainId.RINKEBY]: '0xd41b46F6F1De4341cb02B318c9dF3d9755F59177',
+  [SupportedChainId.HARDHAT]: '0x2279b7a0a67db372996a5fab50d91eaa73d2ebe6',
 };
 export const YMT_ADDRESSES: AddressMap = {
   [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
