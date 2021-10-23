2553ed0d77b23d628d5014ffaf13dbc0a9d584a1
typingmonkey <panebinese@gmail.com>
2021-10-23 23:37:33 +0900

    test(e2e): remove string replace

--- cypress/integration/2-walk-through/main.spec.js
+++ cypress/integration/2-walk-through/main.spec.js
@@ -44,7 +44,7 @@ describe(
       cy.get(currentCollateralAmountSelector)
         .invoke('text')
         .then((before) => {
-          step1Amount = parseFloat(before.replace(/[^0-9]/g, ''));
+          step1Amount = parseFloat(before);
           cy.get(actionAmountSelector).clear();
           cy.get(actionAmountSelector).type(paramEthDelta.toString());
           cy.get(actionTriggerSelector).click();
@@ -52,7 +52,7 @@ describe(
           cy.get(currentCollateralAmountSelector)
             .invoke('text')
             .then((after) => {
-              step2Amount = parseFloat(after.replace(/[^0-9]/g, ''));
+              step2Amount = parseFloat(after);
               expect(step2Amount).eq(step1Amount + paramEthDelta);
             });
         });
@@ -68,7 +68,7 @@ describe(
       cy.get(currentBorrowingAmountSelector)
         .invoke('text')
         .then((before) => {
-          step3Amount = parseFloat(before.replace(/[^0-9]/g, ''));
+          step3Amount = parseFloat(before);
           cy.get(actionAmountSelector).clear();
           cy.get(actionAmountSelector).type(paramCjpyDelta.toString());
           cy.get(actionTriggerSelector).click();
@@ -76,7 +76,7 @@ describe(
           cy.get(currentBorrowingAmountSelector)
             .invoke('text')
             .then((after) => {
-              step4Amount = parseFloat(after.replace(/[^0-9]/g, ''));
+              step4Amount = parseFloat(after);
               expect(step4Amount).eq(step3Amount + paramCjpyDelta);
             });
         });
@@ -93,7 +93,7 @@ describe(
       cy.get(currentBorrowingAmountSelector)
         .invoke('text')
         .then((before) => {
-          step5Amount = parseFloat(before.replace(/[^0-9]/g, ''));
+          step5Amount = parseFloat(before);
           cy.get(actionAmountSelector).clear();
           cy.get(actionAmountSelector).type(repayCjpyDelta.toString());
           cy.get(actionTriggerSelector).click();
@@ -101,7 +101,7 @@ describe(
           cy.get(currentBorrowingAmountSelector)
             .invoke('text')
             .then((after) => {
-              step6Amount = parseFloat(after.replace(/[^0-9]/g, ''));
+              step6Amount = parseFloat(after);
               expect(step6Amount).eq(step5Amount - repayCjpyDelta);
             });
         });
@@ -126,7 +126,7 @@ describe(
         cy.get(currentCollateralAmountSelector)
           .invoke('text')
           .then((before) => {
-            step7Amount = parseFloat(before.replace(/[^0-9]/g, ''));
+            step7Amount = parseFloat(before);
             cy.get(actionAmountSelector).clear();
             cy.get(actionAmountSelector).type(paramEthDelta.toString());
             cy.get(actionTriggerSelector).click();
@@ -134,7 +134,7 @@ describe(
             cy.get(currentCollateralAmountSelector)
               .invoke('text')
               .then((after) => {
-                step8Amount = parseFloat(after.replace(/[^0-9]/g, ''));
+                step8Amount = parseFloat(after);
                 expect(step8Amount).eq(step7Amount - paramEthDelta);
               });
           });
