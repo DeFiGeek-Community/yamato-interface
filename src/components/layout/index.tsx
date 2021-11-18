89d57b5ea258475e439fdf4593a438e392291694
typingmonkey <panebinese@gmail.com>
2021-11-18 23:18:00 +0900

    src/components/layout: remove tablet

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,17 +1,14 @@
 import { useMediaQueryContext } from '../../MediaQueryProvider';
 import LayoutMobile from './media-mobile';
 import LayoutPC from './media-pc';
-import LayoutTablet from './media-tablet';
 
 export default function Layout(props: {
   title?: string;
   children: React.ReactNode;
 }) {
-  const { isMobileSite, isTabletSite, isPcSite } = useMediaQueryContext();
+  const { isMobileSite } = useMediaQueryContext();
   if (isMobileSite) {
     return <LayoutMobile {...props} />;
-  } else if (isTabletSite) {
-    return <LayoutTablet {...props} />;
   } else {
     return <LayoutPC {...props} />;
   }
