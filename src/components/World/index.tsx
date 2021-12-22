17ac37c0e2e74343cc6b47091b64244b2fa7f140
0xTeaTwo <0xteatwo@protonmail.com>
2021-12-22 10:12:21 +0900

    feat(world): add link to yamato contract

--- src/components/World/index.tsx
+++ src/components/World/index.tsx
@@ -1,11 +1,29 @@
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { YAMATO_MAIN_ADDRESSES } from '../../constants/addresses';
+import { useActiveWeb3React } from '../../hooks/web3';
+import { ExplorerDataType, getExplorerLink } from '../../utils/getExplorerLink';
 import { CategoryTitle, ConentBox, HeaderBox1 } from '../CommonItem';
+import { ExternalLink } from '../ExternalLink';
 import LogViewer from './LogViewer';
 
 export default function World() {
+  const { chainId } = useActiveWeb3React();
+
   return (
     <>
       <HeaderBox1>
-        <CategoryTitle>real time TX</CategoryTitle>
+        <CategoryTitle>
+          real time TX{' '}
+          <ExternalLink
+            href={getExplorerLink(
+              chainId ?? 0,
+              YAMATO_MAIN_ADDRESSES[chainId ?? 0],
+              ExplorerDataType.ADDRESS
+            )}
+          >
+            <ExternalLinkIcon />
+          </ExternalLink>
+        </CategoryTitle>
       </HeaderBox1>
       <ConentBox>
         <LogViewer />
