0bfe8ca211a8ad4a1aee8791e813fd4d90fe03df
fullmoonfull <highway.go@gmail.com>
2022-08-12 20:17:16 +0900

    selfRedemption対応

--- src/components/Redemption/SelfRedemption/index.tsx
+++ src/components/Redemption/SelfRedemption/index.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import { useYamatoStateForPledge } from '../../../state/yamato-entirety/hooks';
 import { ItemTitleForPledge } from '../../CommonItem';
 import RedemptionInput from './RedemptionInput';
@@ -6,12 +7,13 @@ import RedemptionInput from './RedemptionInput';
 export default function SelfRedemption() {
   const { redeemableCandidate, rateOfEthJpy, GRR, firstLoadCompleted } =
     useYamatoStateForPledge();
+  const { t } = useTranslation();
 
   return (
     <Grid templateColumns="repeat(6, 1fr)" gap={4} mb={12}>
       <GridItem colSpan={1}>
         <ItemTitleForPledge width="150px" marginTop={26}>
-          償還
+          {t('redemption.selfRedemption.redemption')}
         </ItemTitleForPledge>
       </GridItem>
 
