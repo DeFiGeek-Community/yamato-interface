fcbae911f9614d8ac7843eb92eaabf0c16663ea1
0xHaku <32897506+0xHaku@users.noreply.github.com>
2023-07-29 17:08:39 +0900

    add docs link

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -4,4 +4,5 @@ export const ReferenceList = {
   forum: 'https://gov.defigeek.xyz/',
   discord: 'https://discord.gg/FQYXqVBEnh',
   github: 'https://github.com/DeFiGeek-Community',
+  document: 'https://docs.yamato.fi/',
 };
