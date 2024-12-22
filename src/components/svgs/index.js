c4b92ed24222f5d92c534c088e816c3c14197aa4
naizo10 <naizo018767@gmail.com>
2024-12-22 17:58:32 +0900

    Add ArrowDollar and ArrowEuro to SVG exports

--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -1,5 +1,7 @@
 export { default as CjpyLogo } from './CjpyLogo';
 export { default as ArrowYen } from './ArrowYen';
+export { default as ArrowDollar } from './ArrowDollar';
+export { default as ArrowEuro } from './ArrowEuro';
 export { default as CommunityLogoBlack } from './CommunityLogoBlack';
 export { default as YamatoLogo } from './YamatoLogo';
 export { default as YamatoLogoWithTitle } from './YamatoLogoWithTitle';
