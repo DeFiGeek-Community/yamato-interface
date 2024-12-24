adfa42ae93ed951b39c564cb32b9e7a5aa802408
naizo10 <naizo018767@gmail.com>
2024-12-24 11:52:33 +0900

    Switch from Uniswap to Curve for fetching CJPY price

--- src/state/market/updater.tsx
+++ src/state/market/updater.tsx
@@ -1,6 +1,9 @@
+import { useCurvePoolContract } from '../../hooks/useContract';
 import useInterval from '../../hooks/useInterval';
-import { useActiveWeb3React } from '../../hooks/web3';
-import { fetchCjpyPriceFromUniswap } from '../../utils/fetchState/fetchCjpyPriceFromUniswap';
+import { fetchCjpyPriceFromCurve } from '../../utils/fetchState/fetchCjpyPriceFromCurve';
+// import { useActiveWeb3React } from '../../hooks/web3';
+// import { fetchCjpyPriceFromUniswap } from '../../utils/fetchState/fetchCjpyPriceFromUniswap';
+
 import { rateOfEthJpy } from '../mockData';
 import { useFetchRateOfCjpyEth } from './hooks';
 
@@ -10,7 +13,8 @@ const isUseMock = process.env.REACT_APP_USE_MOCK
 
 // Data except Ethereum
 export default function Updater(): null {
-  const { active, chainId } = useActiveWeb3React();
+  // const { active, chainId } = useActiveWeb3React();
+  const curvePoolContract = useCurvePoolContract();
 
   const dispatchFetchRateOfCjpyEth = useFetchRateOfCjpyEth();
 
@@ -20,8 +24,14 @@ export default function Updater(): null {
         dispatchFetchRateOfCjpyEth('uniswap(v3)', rateOfEthJpy);
         dispatchFetchRateOfCjpyEth('uniswap(v2)', rateOfEthJpy);
       } else {
-        fetchCjpyPriceFromUniswap('v3', chainId, active).then((data) =>
-          dispatchFetchRateOfCjpyEth('uniswap(v3)', data)
+        // Uniswap
+        // fetchCjpyPriceFromUniswap('v3', chainId, active).then((data) =>
+        //   dispatchFetchRateOfCjpyEth('uniswap(v3)', data)
+        // );
+
+        // Curve
+        fetchCjpyPriceFromCurve(curvePoolContract).then((data) =>
+          dispatchFetchRateOfCjpyEth('curve(ETH/CJPY)', data)
         );
       }
     },
