75fe2782251bf10e032f7b46772e4a3ba83b6b6b
0xHaku <32897506+0xHaku@users.noreply.github.com>
2025-01-01 22:24:16 +0900

    remove demo contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,53 +19,6 @@ const Home: NextPage = () => {
           <YamatoStatistics />
           <MyPledge />
           <YamatoFunctions />
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a className={styles.card} href="https://rainbowkit.com">
-              <h2>RainbowKit Documentation &rarr;</h2>
-              <p>Learn how to customize your wallet connection flow.</p>
-            </a>
-
-            <a className={styles.card} href="https://wagmi.sh">
-              <h2>wagmi Documentation &rarr;</h2>
-              <p>Learn how to interact with Ethereum.</p>
-            </a>
-
-            <a
-              className={styles.card}
-              href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-            >
-              <h2>RainbowKit Examples &rarr;</h2>
-              <p>Discover boilerplate example RainbowKit projects.</p>
-            </a>
-
-            <a className={styles.card} href="https://nextjs.org/docs">
-              <h2>Next.js Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a
-              className={styles.card}
-              href="https://github.com/vercel/next.js/tree/canary/examples"
-            >
-              <h2>Next.js Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              className={styles.card}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
         </main>
       </Layout>
     </div>
