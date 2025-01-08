6dae0e1fc052e4c10a981e86d3c7951628acd570
0xHaku <32897506+0xHaku@users.noreply.github.com>
2025-01-08 17:57:55 +0900

    refactor statistics

--- src/hooks/statistics.ts
+++ src/hooks/statistics.ts
@@ -2,7 +2,6 @@ import { YAMATO_MAIN_ADDRESSES } from "@/constants/addresses";
 import { useAppData } from "@/contexts/AppDataContext";
 import { useReadContract } from "wagmi";
 import yamatoABI from "@/constants/abis/yamato/Yamato.json";
-import { useEffect, useState } from "react";
 import { formatUnits } from "viem";
 import { roundDecimal } from "@/utils";
 
@@ -19,36 +18,34 @@ type YamatoStatistics = {
 };
 
 export const useYamatoStatistics = () => {
-  const [data, setData] = useState<YamatoStatistics>({
+  const data: YamatoStatistics = {
     tvl: "",
     tcr: "",
     cjpyTotalSupply: "",
     marketPriceDiff: [],
-  });
+  };
+
   const { chainId, ethPrice } = useAppData();
 
-  const { data: yamatoData, refetch: yamatoRefetch } = useReadContract({
+  const { data: yamatoData } = useReadContract({
     address: YAMATO_MAIN_ADDRESSES[chainId],
     abi: yamatoABI,
     functionName: "getStates",
   }) as { data: bigint[]; refetch: any };
 
-  useEffect(() => {
-    if (yamatoData && ethPrice) {
-      const tcr = roundDecimal(
-        formatUnits(
-          ((yamatoData[0] * ethPrice) / yamatoData[1]) * BigInt(100),
-          18
-        )
-      );
-      setData({
-        tvl: roundDecimal(formatUnits(yamatoData[0] * ethPrice, 18 * 2)),
-        tcr: tcr,
-        cjpyTotalSupply: roundDecimal(formatUnits(yamatoData[1], 18)),
-        marketPriceDiff: [{ poolname: "Curve", value: 0.01 }],
-      });
-    }
-  }, [yamatoData, ethPrice]);
+  if (yamatoData && ethPrice) {
+    const tcr = roundDecimal(
+      formatUnits(
+        ((yamatoData[0] * ethPrice) / yamatoData[1]) * BigInt(100),
+        18
+      )
+    );
+
+    data.tvl = roundDecimal(formatUnits(yamatoData[0] * ethPrice, 18 * 2));
+    data.tcr = tcr;
+    data.cjpyTotalSupply = roundDecimal(formatUnits(yamatoData[1], 18));
+    data.marketPriceDiff = [{ poolname: "Curve", value: 0.01 }];
+  }
 
   return {
     data,
