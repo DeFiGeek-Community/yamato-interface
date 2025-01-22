f83d977fdb411045493234ff3f4f8b1933a0ae1c
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-01-22 17:00:19 +0900

    Update Grid and GridItem properties in CoreRedemption

--- src/components/Redemption/CoreRedemption/index.tsx
+++ src/components/Redemption/CoreRedemption/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { useYamatoStateForPledge } from '../../../state/yamato-entirety/hooks';
 import { ItemTitleForPledge } from '../../CommonItem';
@@ -16,14 +16,28 @@ export default function CoreRedemption() {
   const { t } = useTranslation();
 
   return (
-    <Grid templateColumns="repeat(6, 1fr)" gap={4} mb={12}>
-      <GridItem colSpan={1}>
-        <ItemTitleForPledge width="150px" marginTop={25}>
-          Yamato{t('redemption.coreRedemption.redemption')}
-        </ItemTitleForPledge>
+    <Grid
+      templateColumns={{
+        base: 'repeat(1, 1fr)',
+        md: 'repeat(3, 1fr)',
+      }}
+      gap={4}
+      mb={8}
+    >
+      <GridItem maxWidth={{ base: '100%', md: '300px' }}>
+        <Box my={4}>
+          <ItemTitleForPledge>
+            Yamato{t('redemption.coreRedemption.redemption')}
+          </ItemTitleForPledge>
+        </Box>
       </GridItem>
 
-      <GridItem colSpan={5}>
+      <GridItem
+        colSpan={{
+          base: 1,
+          md: 2,
+        }}
+      >
         <RedemptionInput
           rateOfEthJpy={rateOfEthJpy}
           redemptionReserve={redemptionReserve}
