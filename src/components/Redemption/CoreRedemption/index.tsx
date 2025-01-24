42b384f8d128aea23e5cd28cfbe8b87b28948825
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-01-24 11:44:14 +0900

    Update responsive layout for redemption input form

--- src/components/Redemption/CoreRedemption/index.tsx
+++ src/components/Redemption/CoreRedemption/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Box } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { useYamatoStateForPledge } from '../../../state/yamato-entirety/hooks';
 import { ItemTitleForPledge } from '../../CommonItem';
@@ -16,26 +16,28 @@ export default function CoreRedemption() {
   const { t } = useTranslation();
 
   return (
-    <Grid
+    <Grid 
       templateColumns={{
         base: 'repeat(1, 1fr)',
-        md: 'repeat(3, 1fr)',
-      }}
+        md: 'repeat(12, 1fr)'
+      }} 
       gap={4}
-      mb={8}
     >
-      <GridItem maxWidth={{ base: '100%', md: '300px' }}>
-        <Box my={4}>
-          <ItemTitleForPledge>
-            Yamato{t('redemption.coreRedemption.redemption')}
-          </ItemTitleForPledge>
-        </Box>
-      </GridItem>
-
-      <GridItem
+      <GridItem 
         colSpan={{
           base: 1,
-          md: 2,
+          md: 2
+        }}
+      >
+        <ItemTitleForPledge width="150px" marginTop={24}>
+          Yamato{t('redemption.coreRedemption.redemption')}
+        </ItemTitleForPledge>
+      </GridItem>
+
+      <GridItem 
+        colSpan={{
+          base: 1,
+          md: 10
         }}
       >
         <RedemptionInput
