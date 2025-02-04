67b8614683d28af231c60668400b9b39f6b4785b
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-02-04 19:46:04 +0900

    Add function to set language from URL parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import i18next from 'i18next';
 import { Switch, Route } from 'react-router-dom';
 import './i18n/configs';
 import Web3ReactManager from './components/Web3ReactManager';
@@ -11,6 +11,19 @@ import PledgeUpdater from './state/pledge/updater';
 import TransactionsUpdater from './state/transactions/updater';
 import WalletUpdater from './state/wallet/updater';
 import YamatoEntiretyUpdater from './state/yamato-entirety/updater';
+
+// URLから言語を取得する関数
+function getLanguageFromUrl(): string | null {
+  const hash = window.location.hash;
+  const params = new URLSearchParams(hash.slice(hash.indexOf('?')));
+  return params.get('lang');
+}
+
+// 初期化時にURLパラメータで言語を設定
+const language = getLanguageFromUrl();
+if (language && (language === 'en' || language === 'ja')) {
+  i18next.changeLanguage(language);
+}
 
 function App() {
   return (
